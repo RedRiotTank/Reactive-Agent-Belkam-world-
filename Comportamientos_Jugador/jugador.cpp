e03fbf41c43ae046df09d7d79b416ee0f197ac50
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2022-03-23 20:27:13 +0100

    change: priority movement

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -5,6 +5,120 @@
 #include<fstream>
 using namespace std;
 
+void ComportamientoJugador::CrearMapaPotencialPrioritario(Sensores sensores, int numCasilla, int brujula){ //supone que no hay obstaculos como muros o precipicios.
+	int haciaAdelante = 0, haciaLado = 0;
+
+
+	int potencialCampo = 1;
+
+
+	//haciaAdelante:
+	if(numCasilla >= 9 and numCasilla <= 15){
+		haciaAdelante = 3;
+	} else if (numCasilla >= 4 and numCasilla <=8){
+		haciaAdelante = 2;
+	} else if(numCasilla >= 1 and numCasilla <=3) {
+		haciaAdelante = 1;
+	}
+
+	//haciaLado:
+
+	if(numCasilla == 15 || numCasilla == 9)
+		haciaLado = 3;
+	else if(numCasilla == 10 || numCasilla ==4 || numCasilla == 14 || numCasilla == 8 )
+		haciaLado = 2;
+	else if(numCasilla == 1 || numCasilla == 5 || numCasilla == 11 || numCasilla == 3 || numCasilla == 7 || numCasilla == 13)
+		haciaLado = 1;
+
+	//Diseño del mapa:
+
+	//haciaadelante:
+
+	for(int i=1; i<=haciaAdelante; i++){
+		switch (brujula){
+			case 0:	//norte ^
+
+				mapaPotencial[filPotPrio - i][colPotPrio] = potencialCampo;
+
+			break;
+			
+			case 1: //este --> 
+				mapaPotencial[filPotPrio][colPotPrio + i] = potencialCampo;
+			break;
+
+			case 2:	//sur V
+				mapaPotencial[filPotPrio + i][colPotPrio] = potencialCampo;
+			break;
+
+			case 3: //oeste <--
+				mapaPotencial[filPotPrio][colPotPrio - i] = potencialCampo;
+			break;
+		}
+	
+		potencialCampo++;
+	}
+
+
+	//Comprobamos si está a la izq,dcha o centro.
+
+	bool izq = false, dcha = false, ctr = false;
+
+	if(numCasilla == 1 or numCasilla == 4 or numCasilla == 5 or numCasilla == 9 or numCasilla == 10 or numCasilla == 11)
+		izq = true;
+	else if(numCasilla == 3 or numCasilla == 7 or numCasilla == 8 or numCasilla == 13 or numCasilla == 14 or numCasilla == 15)
+		dcha = true;
+	else
+		ctr = true;
+
+	//haciaLado:
+
+	int factorOrient = 0;	// sirve para determinar posiciones en funcion de si el objeto está a la izq, a la derecha, o en el centro
+
+	if(dcha)
+		factorOrient = 1;
+	else if (izq)
+		factorOrient = -1;
+
+
+	if(!ctr){
+
+		for(int i=1; i<=haciaLado; i++){
+		switch (brujula){
+			case 0:	//norte ^
+
+				mapaPotencial[filPotPrio - haciaAdelante][ colPotPrio + (factorOrient*i)] = potencialCampo;
+
+			break;
+			
+			case 1: //este --> 
+				mapaPotencial[filPotPrio + (factorOrient*i)][colPotPrio + haciaAdelante] = potencialCampo;
+			break;
+
+			case 2:	//sur V
+
+				factorOrient = factorOrient * -1; // para invertirlo.
+				mapaPotencial[filPotPrio + haciaAdelante][colPotPrio + (factorOrient*i)] = potencialCampo;
+				factorOrient = factorOrient * -1; //para re invertirlo (pq si no s eestá cambiando todo el rato).
+			break;
+
+			case 3: //oeste <--
+				factorOrient = factorOrient * -1;
+				mapaPotencial[filPotPrio + (factorOrient*i)][colPotPrio - haciaAdelante] = potencialCampo;
+				factorOrient = factorOrient * -1;
+			break;
+		}
+	
+		potencialCampo++;
+		}
+
+	}
+	
+
+	
+	
+	
+	}
+
 void ComportamientoJugador::pM(Sensores sensores, vector<vector<unsigned char>> &map, int &fil, int &col){
 
 	map[fil][col] = sensores.terreno[0];
@@ -141,6 +255,35 @@ void ComportamientoJugador::crearArchivoMatrizAux(Sensores sensores){
 	//-----
 }
 
+
+void ComportamientoJugador::crearArchivoMapaPot(Sensores sensores){
+
+	//-----
+		ofstream archivo;
+		archivo.open("mapapotencial.txt",ios::out);
+		if(archivo.fail()){
+			cout << "ERROR AL CREAR EL ARCHIVO";
+			exit(1);
+		} else {
+
+
+			
+			for(int i=0; i<9; i++){
+				for(int j=0; j<9; j++){
+					archivo<<mapaPotencial[i][j] << " ";
+				}
+				archivo << endl;
+			}
+
+
+			
+
+		}
+		archivo.close();
+
+	//-----
+}
+
 void ComportamientoJugador::actualizarPosYBruj(Sensores sensores, int &fil, int &col){
 	
 		switch(ultimaAccion){
@@ -170,203 +313,24 @@ void ComportamientoJugador::actualizarPosYBruj(Sensores sensores, int &fil, int 
 
 }
 
+void ComportamientoJugador::resetPrioritario(){
+	protocoloPrioritario = false;
+	 for(int i=0; i<8; i++)
+        for(int j=0; j<8; j++)
+          mapaPotencial[i][j] = 0;
 
+	filPotPrio = colPotPrio = 4;
 
-pair<bool,int> ComportamientoJugador::detectoObjetoPrioritario(Sensores sensores){
-	pair<bool,int> res;
-	
+}
+
+int ComportamientoJugador::detectarObjetoPrioritario(Sensores sensores){
 	//bikini, zapatillas y posicionamiento, RECARGA NO.
 
-
-	//bikini
-	if(!tengoBikini){
-		//lado izq
-		for(int i=1; i <= 11; i++){
-
-			if(sensores.terreno[i] == 'K'){
-				res.first = true;
-				res.second = i;
-				return res;
-			}
-
-			if(i == 1)
-				i = 3;
-
-			if(i == 5)
-				i == 8;
-		}
-
-		//centro
-		if(sensores.terreno[2] == 'K'){
-			res.first = true;
-			res.second = 2;
-			return res;
-		}
-
-		if(sensores.terreno[6] == 'K'){
-			res.first = true;
-			res.second = 6;
-			return res;
-		}
-
-		if(sensores.terreno[12] == 'K'){
-			res.first = true;
-			res.second = 12;
-			return res;
-		}
-
-		// derecho
-		for(int i=3; i <= 11; i++){
-
-			if(sensores.terreno[i] == 'K'){
-				res.first = true;
-				res.second = i;
-				return res;
-			}
-
-			if(i == 3)
-				i = 6;
-
-			if(i == 8)
-				i == 12;
-
-		}
-	}
-	
-	if(sensores.terreno[0] == 'K'){
-		res.first = false;
-		res.first = 0;
-		return res;
-	}
-
-	//zapatillas:
-	if(!tengoZapas){
-		//lado izq
-		for(int i=1; i <= 11; i++){
-
-			if(sensores.terreno[i] == 'D'){
-				res.first = true;
-				res.second = i;
-				return res;
-			}
-
-			if(i == 1)
-				i = 3;
-
-			if(i == 5)
-				i == 8;
-
-		}
-
-		//centro
-		if(sensores.terreno[2] == 'D'){
-			res.first = true;
-			res.second = 2;
-			return res;
-		}
-
-		if(sensores.terreno[6] == 'D'){
-			res.first = true;
-			res.second = 6;
-			return res;
-		}
-
-		if(sensores.terreno[12] == 'D'){
-			res.first = true;
-			res.second = 12;
-			return res;
-		}
-
-		//lado derecho
-
-	
-		for(int i=3; i <= 11; i++){
-			if(sensores.terreno[i] == 'D'){
-				res.first = true;
-				res.second = i;
-				return res;
-			}
-
-			if(i == 3)
-				i = 6;
-
-			if(i == 8)
-				i == 12;
-		}
-	}
-	
-	if(sensores.terreno[0] == 'D'){
-		res.first = false;
-		res.first = 0;
-		return res;
-	}
-
-	//posicionamiento
-	if(!bien_situado){
-		//lado izq
-		for(int i=1; i <= 11; i++){
-
-			if(sensores.terreno[i] == 'G'){
-				res.first = true;
-				res.second = i;
-				return res;
-			}
-
-			if(i == 1)
-				i = 3;
-
-			if(i == 5)
-				i == 8;
-
-		}
-
-		//centro
-		if(sensores.terreno[2] == 'G'){
-			res.first = true;
-			res.second = 2;
-			return res;
-		}
-
-		if(sensores.terreno[6] == 'G'){
-			res.first = true;
-			res.second = 6;
-			return res;
-		}
-
-		if(sensores.terreno[12] == 'G'){
-			res.first = true;
-			res.second = 12;
-			return res;
-		}
-
-		//lado derecho
-
-	
-		for(int i=3; i <= 11; i++){
-			if(sensores.terreno[i] == 'G'){
-				res.first = true;
-				res.second = i;
-				return res;
-			}
-
-			if(i == 3)
-				i = 6;
-
-			if(i == 8)
-				i == 12;
-		}
-	}
-	
-	if(sensores.terreno[0] == 'G'){
-		res.first = false;
-		res.first = 0;
-		return res;
-	}
-
-	res.first = false;
-	res.second = -1;
-
-	return res;
+	if(!protocoloPrioritario)
+		for(int i=1; i<= 15; i++)
+			if((sensores.terreno[i] == 'K' && !tengoBikini) or (sensores.terreno[i] == 'D' && !tengoZapas) or sensores.terreno[i] == 'G' && !bien_situado )
+				return i;
+	return -1;
 }
 
 Action ComportamientoJugador::movimientoDefault(Sensores sensores){
@@ -384,65 +348,85 @@ Action ComportamientoJugador::movimientoDefault(Sensores sensores){
 	}
 }
 
-Action ComportamientoJugador::movimientoPrioritario(Sensores sensores){	//Sistema de producción.
-pair<bool,int> prio = detectoObjetoPrioritario(sensores);
+Action ComportamientoJugador::movimientoPrioritario(Sensores sensores, int brujula){	//Sistema de producción.
 
-		if (prio.first == true)		//Siempre que lo vea delante de el, avanzará
-			return actFORWARD;
-		else {
+	Action resultado = actIDLE;
 
-			if(prio.first == false){
-				if(PrioIzq){
-					PrioIzq = false;
-					return actTURN_L;
+	mapaPotencial[filPotPrio][colPotPrio] = 0;
+	
+	//movimiento de avance
+		
+		switch (brujula){
+				case 0:	//norte
+
+					if(mapaPotencial[filPotPrio][colPotPrio-1] > mapaPotencial[filPotPrio][filPotPrio])
+						resultado = actTURN_L;
+					else if(mapaPotencial[filPotPrio][colPotPrio+1] > mapaPotencial[filPotPrio][filPotPrio])
+						resultado = actTURN_R;
+					else if (mapaPotencial[filPotPrio - 1][colPotPrio] > mapaPotencial[filPotPrio][filPotPrio]){
+						resultado = actFORWARD;
+						filPotPrio--;
+					}
 					
-				}
+				break;
+				case 1://este
+					
+					if(mapaPotencial[filPotPrio-1][colPotPrio] > mapaPotencial[filPotPrio][filPotPrio]){
 
-				if(prioDcha){
-					prioDcha = false;
-					return actTURN_R;
-				}
-			}
+						resultado = actTURN_L;
+					}
+					else if(mapaPotencial[filPotPrio+1][colPotPrio] > mapaPotencial[filPotPrio][filPotPrio]){
+						
+						resultado = actTURN_R;
+					}
+					else if (mapaPotencial[filPotPrio][colPotPrio + 1] > mapaPotencial[filPotPrio][filPotPrio]){
+						
+						resultado = actFORWARD;
+						colPotPrio++;
+						
+					}
+				break;
+
+				case 2://sur
+					if(mapaPotencial[filPotPrio][colPotPrio-1] > mapaPotencial[filPotPrio][filPotPrio])
+						resultado = actTURN_R;
+					else if(mapaPotencial[filPotPrio][colPotPrio+1] > mapaPotencial[filPotPrio][filPotPrio])
+						resultado = actTURN_L;
+					else if(mapaPotencial[filPotPrio+1] [colPotPrio] > mapaPotencial[filPotPrio][filPotPrio]){
+						resultado = actFORWARD;
+						filPotPrio++;
+						
+					}
+
+				break;
+
+				case 3://oeste
+					
+					if(mapaPotencial[filPotPrio-1][colPotPrio] > mapaPotencial[filPotPrio][filPotPrio]){
+						
+						resultado = actTURN_R;
+					}
+					else if(mapaPotencial[filPotPrio+1][colPotPrio] > mapaPotencial[filPotPrio][filPotPrio]){
+						
+						resultado = actTURN_L;
+					}
+					else if (mapaPotencial[filPotPrio][colPotPrio - 1] > mapaPotencial[filPotPrio][filPotPrio]){
+						
+						resultado = actFORWARD;
+						colPotPrio--;
+						
+					}
+				break;
+
 		}
 
-		if(prio.second == 0)
-			protocoloPrioritario = false;
+	return resultado;
 
-		if(sensores.terreno[0] == 'K')
-			tengoBikini = true;
-
-		if(sensores.terreno[0] == 'D')
-			tengoZapas = true;	
 
 }
 
-void ComportamientoJugador::seleccionPosicionPrioritario(Sensores sensores){
-
-	if(!protocoloPrioritario){
-
-		pair<bool,int> prot;
-		prot = detectoObjetoPrioritario(sensores);
-
-		if(prot.first == true){
-			
-			protocoloPrioritario = true;
-
-			if (prot.second == 1 || prot.second == 4 || prot.second == 5 
-			|| prot.second == 9 || prot.second == 10 || prot.second == 11)
-			PrioIzq = true;
-
-			else if (prot.second == 3 || prot.second == 7 || prot.second == 8
-			|| prot.second == 13 || prot.second == 14 || prot.second == 15)
-			prioDcha = true;
-
-			else if (prot.second == 2 || prot.second == 6 || prot.second == 12)
-			PrioCentro = true;
-		}
-	}
-}
 
 Action ComportamientoJugador::think(Sensores sensores){
-
 
 	if(sensores.nivel == 0 && primiter){
 		bien_situado = true;
@@ -456,21 +440,25 @@ Action ComportamientoJugador::think(Sensores sensores){
 	if(bien_situado){
 		
 		actualizarPosYBruj(sensores,fil,col);
-		cout << " B" << endl;
 		pM(sensores,mapaResultado,fil,col);
-		cout << " C" << endl;
+
 	}
 	else{
 		
 		actualizarPosYBruj(sensores,filAux,colAux);
-		
 		pM(sensores,mapaAuxiliar,filAux,colAux);
 	}
 
-	
-
 
 	crearArchivoMatrizAux(sensores);
+
+	//comprobar si he cogido bikini o zapas o punto de control:
+
+	if(sensores.terreno[0] == 'D')
+		tengoZapas = true;
+	
+	if(sensores.terreno[0] == 'K')
+		tengoBikini = true;
 
 	if(sensores.terreno[0] == 'G' && !bien_situado){
 		
@@ -478,18 +466,39 @@ Action ComportamientoJugador::think(Sensores sensores){
 		bien_situado = true;
 	}
 
+	//desactivar el protocolo prioritario:
 
-	seleccionPosicionPrioritario(sensores);
-	
+	if(sensores.terreno[0] == 'K' || sensores.terreno[0] == 'G' || sensores.terreno[0] == 'D')
+		resetPrioritario();
+
+
 
 	//Decidir la nueva accion
 
-	accion = movimientoDefault(sensores);
+	if(!protocoloPrioritario)
+		casillaSensorPrioritaria = detectarObjetoPrioritario(sensores);
+
+	if(casillaSensorPrioritaria != -1 && !protocoloPrioritario){
+		protocoloPrioritario = true;
+		CrearMapaPotencialPrioritario(sensores,casillaSensorPrioritaria,brujula);
+		crearArchivoMapaPot(sensores);
+
+	}
+
+	crearArchivoMapaPot(sensores);
+
+	
 
 	if(protocoloPrioritario){
-		accion = movimientoPrioritario(sensores);
-			
+			cout << "P" << endl;
+		accion = movimientoPrioritario(sensores,brujula);	//seguir mapa de potencial en vez de mapa normal.
 	}
+	 else {
+		cout << "N" << endl;
+		accion = movimientoDefault(sensores);
+
+	 }
+	
 
 //----------------------------------------------------------------------
 	// Determinar el efecto de la ultima accion enviada
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -10,21 +10,23 @@ using namespace std;
 class ComportamientoJugador : public Comportamiento{
   public:
     void pM(Sensores sensores, vector<vector<unsigned char>> &map, int &fil,int &col) ;
-
+    void resetPrioritario();
     void pasarMapaAuxAmapa(Sensores sensores);
     void actualizarPosYBruj(Sensores sensores,int &fil, int &col);
-    pair<bool,int> detectoObjetoPrioritario(Sensores sensores);  
+    int detectarObjetoPrioritario(Sensores sensores);  
+    void crearArchivoMapaPot(Sensores sensores);
 
-    void seleccionPosicionPrioritario(Sensores sensores);
+    void CrearMapaPotencialPrioritario(Sensores sensores, int numCasilla,int brujula);
+    
     void crearArchivoMatrizAux(Sensores sensores);
     Action movimientoDefault(Sensores sensores);
-    Action movimientoPrioritario(Sensores sensores);
+    Action movimientoPrioritario(Sensores sensores, int brujula);
 
 
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
       // Dar el valor inicial a las variables de estado
-
+      casillaSensorPrioritaria = -1;
       ultimaAccion = actIDLE;
       tengoZapas = tengoBikini = false;
       
@@ -48,7 +50,21 @@ class ComportamientoJugador : public Comportamiento{
         for(int j=0; j<200; j++)
           mapaAuxiliar[i][j] = '?';
       
+
+      //mapa potencial
+     
+      filPotPrio = colPotPrio = 4; //en mitad del mapa, para que haya espacio de sobra.
+
+      mapaPotencial = vector<vector<int>>(9,vector<int>(9));
+
+      for(int i=0; i<9; i++)
+        for(int j=0; j<9; j++)
+          mapaPotencial[i][j] = 0;
       
+
+
+
+
 
      primiter = true;
 
@@ -73,9 +89,11 @@ class ComportamientoJugador : public Comportamiento{
   
   // Declarar aquí las variables de estado
     vector<vector<unsigned char>> mapaAuxiliar;
+    vector<vector<int>> mapaPotencial;
     int fil, col;
+    int filPotPrio,colPotPrio;
     int filAux, colAux, brujula;
-
+    int casillaSensorPrioritaria;
 
     bool girar_derecha;
     bool tengoZapas, tengoBikini;
@@ -84,8 +102,8 @@ class ComportamientoJugador : public Comportamiento{
     bool primeraDeteccionObjetoPrioritario;
     bool primiter;
     Action ultimaAccion;
+    bool seHaGiradoPorPrioridad = false;
     bool bien_situado; //nos dice si conocemos las coordenadas en las que estamos.
-
 
 };
 
